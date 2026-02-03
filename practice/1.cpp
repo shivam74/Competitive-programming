8c2a5cf666a046a3950095ddd382ae07338a40bd
Shivam <shivamraghuwanshi741@gmail.com>
2026-02-03 20:42:54 +0530

    problems updated

--- practice/1.cpp
+++ practice/1.cpp
@@ -1,83 +1,38 @@
- #include <bits/stdc++.h>
- using namespace std;
- 
- #define ll long long
- #define INF 5*1e18
- typedef unsigned long long ull;
- typedef long double lld;
- 
- #define test ll t; cin >> t; while(t--)
- #define vll vector<ll>
- #define all(v) v.begin(),v.end()
- #define fl(i,f,d) for(ll i=f;i<=d;i++)
- #define rl(i,f,d) for(ll i=f;i>=d;i--)
- #define nl "\n"
- 
- #include <ext/pb_ds/assoc_container.hpp>
- #include <ext/pb_ds/tree_policy.hpp>
- using namespace __gnu_pbds;
- 
- // Ordered Set Template (Ordered multiset)
- //ordered_set s;
- //s.order_of_key(x) it give no. of elements less than x
- //s.find_by_order(i) (i->[0-(n-1)]) it give the iterator of i'th element in the set
- typedef tree<
-     long long, //pii(pair<ll,ll>
-     null_type,
-     less<long long>,//less<pii>
-     rb_tree_tag,
-     tree_order_statistics_node_update
- > ordered_set;//ordered_multiset
- 
- 
- 
- bool isprime(ll x){
-     if (x < 2) return false;
-     if (x == 2) return true;
-     if (x % 2 == 0) return false;
-     for(ll i = 3; i * i <= x; i += 2){
-         if (x % i == 0) return false;
-     }
-     return true;
- }
- 
- ll mod = 1e9+7;
- vector<ll> primeFactors(ll n) {
-     vector<ll> factors;
-     while (n % 2 == 0) { factors.push_back(2); n /= 2; }
-     for(ll i=3; i*i<=n; i+=2){
-         while(n % i == 0){ factors.push_back(i); n /= i; }
-     }
-     if(n != 1) factors.push_back(n);
-     return factors;
- }
- 
- ll modExp(ll a, ll b, ll mod){
-      a%=mod;
-       ll res=1;              
-       while(b){
-           if(b & 1){
-               res=res*a %mod;
-           }
-           a=a*a % mod;
-           b/=2;
-       }
-       return res;
- }
- long long modInverse(long long a, long long mod) {
-     return modExp(a, mod - 2, mod);
- }
- 
- //------------------------Solution starts from here------------------------
- 
- void solve(){
-    
- }
- 
- int32_t main(){
-     ios_base::sync_with_stdio(false);
-     cin.tie(NULL);
-     test
-         solve();
-     return 0;
- }
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int inf = (int)1e9;
+const int N = 777;
+
+int c[N];
+int f[N][N];
+
+int main() {
+  int n;
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++) {
+    scanf("%d", c + i);
+  }
+  for (int i = n - 1; i >= 0; i--) {
+    for (int j = i; j < n; j++) {
+      if (i == j) {
+        f[i][j] = 1;
+        continue;
+      }
+      if (i + 1 == j) {
+        f[i][j] = (c[i] == c[j] ? 1 : 2);
+        continue;
+      }
+      f[i][j] = inf;
+      for (int k = i; k < j; k++) {
+        f[i][j] = min(f[i][j], f[i][k] + f[k + 1][j]);
+      }
+      if (c[i] == c[j]) {
+        f[i][j] = min(f[i][j], f[i + 1][j - 1]);
+      }
+    }
+  }
+  printf("%d\n", f[0][n - 1]);
+  return 0;
+}
---
+++ practice/A_Mex_in_the_Grid.cpp
@@ -0,0 +1,144 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define INF 5*1e18
+typedef unsigned long long ull;
+typedef long double lld;
+
+#define test ll t; cin >> t; while(t--)
+#define vll vector<ll>
+#define all(v) v.begin(),v.end()
+#define fl(i,f,d) for(ll i=f;i<=d;i++)
+#define rl(i,f,d) for(ll i=f;i>=d;i--)
+#define nl "\n"
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+// Ordered Set Template (Ordered multiset)
+//ordered_set s;
+//s.order_of_key(x) it give no. of elements less than x
+//s.find_by_order(i) (i->[0-(n-1)]) it give the iterator of i'th element in the set
+typedef tree<
+    long long, //pii(pair<ll,ll>
+    null_type,
+    less<long long>,//less<pii>
+    rb_tree_tag,
+    tree_order_statistics_node_update
+> ordered_set;//ordered_multiset
+
+
+
+bool isprime(ll x){
+    if (x < 2) return false;
+    if (x == 2) return true;
+    if (x % 2 == 0) return false;
+    for(ll i = 3; i * i <= x; i += 2){
+        if (x % i == 0) return false;
+    }
+    return true;
+}
+
+ll mod = 1e9+7;
+vector<ll> primeFactors(ll n) {
+    vector<ll> factors;
+    while (n % 2 == 0) { factors.push_back(2); n /= 2; }
+    for(ll i=3; i*i<=n; i+=2){
+        while(n % i == 0){ factors.push_back(i); n /= i; }
+    }
+    if(n != 1) factors.push_back(n);
+    return factors;
+}
+
+ll modExp(ll a, ll b, ll mod){
+     a%=mod;
+      ll res=1;              
+      while(b){
+          if(b & 1){
+              res=res*a %mod;
+          }
+          a=a*a % mod;
+          b/=2;
+      }
+      return res;
+}
+long long modInverse(long long a, long long mod) {
+    return modExp(a, mod - 2, mod);
+}
+
+//------------------------Solution starts from here------------------------
+
+void solve(){
+    ll n; cin>>n;
+    vector<vector<ll>> v(n+1,vector<ll>(n+1,-1));
+    ll cur=0;
+    ll x=n*n-1;
+    ll dir=0;//0-right , 1-left , 2-up , 3-down
+    ll i=(n+1)/2,j=(n+1)/2;
+    v[i][j]=cur;
+    j++;
+    cur++;
+    while(cur<=x){
+        if(dir==0){
+            v[i][j]=cur;
+            cur++;
+            while(v[i+1][j]!=-1 && i<=n && j<=n){
+                j++;
+                v[i][j]=cur;
+                cur++;
+            }
+            i++;
+            dir=3;
+        }
+        else if(dir==1){
+            v[i][j]=cur;
+            cur++;
+            while(v[i-1][j]!=-1 && i>=1 && j>=1){
+                j--;
+                v[i][j]=cur;
+                cur++;
+            }
+            i--;
+            dir=2;
+        }
+        else if(dir==2){
+            v[i][j]=cur;
+            cur++;
+            while(v[i][j+1]!=-1 && i<=n && j<=n){
+                i--;
+                v[i][j]=cur;
+                cur++;
+            }
+            j++;
+            dir=0;
+        }
+        else if(dir==3){
+            v[i][j]=cur;
+            cur++;
+            while(v[i][j-1]!=-1 && i<=n && j<=n){
+                i++;
+                v[i][j]=cur;
+                cur++;
+            }
+            j--;
+            dir=1;
+        }
+    }
+    for(ll i=1;i<=n;i++){
+        for(ll j=1;j<=n;j++){
+            cout<<v[i][j]<<" ";
+        }
+        cout<<nl;
+    }
+
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    test
+        solve();
+    return 0;
+}
